9b66e7cf9569fbb58ff01a327dcec27b950882d7
Bevan Koopman <bevan@koopman.id.au>
2021-09-13 09:47:19 +1000

    Write collection in one pass (more efficient).

--- examples/agask/helpers/build_collection_from_index.py
+++ examples/agask/helpers/build_collection_from_index.py
@@ -3,20 +3,22 @@ from pyserini.search import SimpleSearcher
 from tqdm import tqdm
 
 
+def write_doc_to_collection(docid, searcher: SimpleSearcher, out_file):
+    text = searcher.doc(docid).contents().replace("\n", " ").replace("\t", " ")
+    out_file.write(f"{docid}\t{text}\n")
+
+
 def build_collection(index_dir, collection_outfile, qrels=None):
     searcher = SimpleSearcher(index_dir)
 
-    docs = []
-    if qrels:
-        with open(qrels) as qfh:
-            docs = [line.split()[2] for line in qfh]
-    else:
-        docs = [searcher.doc(i).docid() for i in range(searcher.num_docs)]
-
     with open(collection_outfile, 'w') as out_file:
-        for docid in tqdm(docs, desc=f"Outputting {len(docs)} docs"):
-            text = searcher.doc(docid).contents().replace("\n", " ").replace("\t", " ")
-            out_file.write(f"{docid}\t{text}\n")
+        if qrels:
+            with open(qrels) as qfh:
+                for line in tqdm(qfh, f"Writing docs from {qrels}"):
+                    write_doc_to_collection(line.split()[2], searcher, out_file)
+        else:
+            for i in tqdm(range(searcher.num_docs), desc=f"Outputting {searcher.num_docs} docs"):
+                write_doc_to_collection(searcher.doc(i).docid(), searcher, out_file)
 
 
 if __name__ == '__main__':

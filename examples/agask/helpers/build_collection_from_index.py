063d66fb98aa59238b94b58d5adc76abd3aa4434
Bevan Koopman <bevan@koopman.id.au>
2022-08-31 09:30:58 +1000

    Now generates CSV of all docs that is needed by feature generation pipeline.

--- examples/agask/helpers/build_collection_from_index.py
+++ examples/agask/helpers/build_collection_from_index.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 from pyserini.search import SimpleSearcher
 from tqdm import tqdm
 
@@ -8,26 +9,38 @@ def write_doc_to_collection(docid, searcher: SimpleSearcher, out_file):
     out_file.write(f"{docid}\t{text}\n")
 
 
-def build_collection(index_dir, collection_outfile, qrels=None):
+def write_doc_to_docs(docid, searcher: SimpleSearcher, out_file):
+    doc_json = json.loads(searcher.doc(docid).raw())
+    items = [doc_json[field].replace(',', '') for field in ['report_id', 'pdf_url', 'report_title', 'text']]
+    out_file.write(f"{','.join(items)}\n")
+
+
+def build_collection(index_dir, outfile, qrels=None, docs_format=True):
     searcher = SimpleSearcher(index_dir)
 
-    with open(collection_outfile, 'w') as out_file:
+    with open(outfile, 'w') as out_file:
         if qrels:
             with open(qrels) as qfh:
                 for line in tqdm(qfh, f"Writing docs from {qrels}"):
                     write_doc_to_collection(line.split()[2], searcher, out_file)
         else:
             for i in tqdm(range(searcher.num_docs), desc=f"Outputting {searcher.num_docs} docs"):
-                write_doc_to_collection(searcher.doc(i).docid(), searcher, out_file)
+                if docs_format:
+                    write_doc_to_docs(searcher.doc(i).docid(), searcher, out_file)
+                else:
+                    write_doc_to_collection(searcher.doc(i).docid(), searcher, out_file)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--index_dir', required=True, help="Anserini index")
-    parser.add_argument('--collection', help="File to write collection", required=True)
+    parser.add_argument('-o', '--outfile', help="File to write output", required=True)
     parser.add_argument('--qrels', help="Use this qrel file for docs")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--collection', action='store_true', help='Write in collection format.')
+    group.add_argument('--docs', action='store_true', help='Write in docs format')
 
     args = parser.parse_args()
 
-    build_collection(index_dir=args.index_dir, collection_outfile=args.collection, qrels=args.qrels)
+    build_collection(index_dir=args.index_dir, outfile=args.outfile, qrels=args.qrels, docs_format=args.docs)
     print(f"Collection written to {args.collection}")
